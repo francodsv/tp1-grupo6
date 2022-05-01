612949a26cb0755a9ec8f53d9b50614f7e2fc155
coliin7 <colorsanguineti@ctrl365.com>
2022-05-01 12:21:58 -0300

    Se agrega clase "Comentario.cs" y "Reaccion.cs".

---
+++ tp1-grupo6/Logica/Reaccion.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp1_grupo6.Logica
+{
+    class Reaccion
+    {
+        public int ID { get; set; }
+        public Enum Tipo { get; set; }
+        public Post Post { get; set; }
+        public Usuario Usuario { get; set; }
+
+
+        public Reaccion(int ID, Enum Tipo, Post Post, Usuario Usuario)
+        {
+            this.ID = ID;
+            this.Tipo = Tipo;
+            this.Post = Post;
+            this.Usuario = Usuario;
+        }
+    }
+}
