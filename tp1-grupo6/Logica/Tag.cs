4603f34f07b5e57d8c5d18cfee7da5830161c004
Pablo <pablo.munoz@davinci.edu.ar>
2022-05-02 21:15:43 -0300

    Se agregan clases Usuario y Tag , restan Metodos de respectivas clases

---
+++ tp1-grupo6/Logica/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp1_grupo6.Logica
+{
+    internal class Tag
+    {
+        public int ID    { get; set; }
+        public String Palabra { get; set; }
+
+        public List<Post> Posts { get; set; }
+
+
+        public Tag(){}
+        public Tag(int ID, String Palabra) {
+         
+            this.ID = ID;
+            this.Palabra = Palabra;
+
+
+
+
+        }
+}
+}
---
+++ tp1-grupo6/Logica/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp1_grupo6.Logica
+{
+     class Usuario
+    {
+        public int ID { get; set; }
+
+        public int DNI { get; set; }
+
+        public String Nombre{ get; set; }
+
+        public String Apellido  { get; set; }
+        public String Mail { get; set; }
+
+        public String Password { get; set; }
+
+        public int IntentosFallidos { get; set; }
+        public bool Bloqueado { get; set; }
+        public List<Usuario> Amigos { get; set; }
+        public List <Post> MisPosts { get; set; }
+        public List<Comentario> MisComentarios { get; set; }
+        public List<Reacciones> MisReacciones { get; set; }
+
+        //creeria que el constructor no necesita crearse con reacciones , comentarios , posts y usuarios
+
+        public Usuario(){ }
+        public Usuario (int ID,int DNI, String Nombre, String Apellido, String Mail, String Password, int IntentosFallidos, bool Bloqueado)
+        {
+            this.ID = ID;
+            this.DNI = DNI;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Mail=Mail;
+            this.Password = Password;
+            this.IntentosFallidos = IntentosFallidos;
+            this.Bloqueado = Bloqueado;
+
+        }
+
+    }
+}
