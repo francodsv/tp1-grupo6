ec7eb38f2b015429501ff5335f4ba27a23c71564
Franco de San Vicente <francodv31@hotmail.com>
2022-05-08 23:11:46 -0300

    test

---
+++ tp1-grupo6/Front/index.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tp1_grupo6.Front
+{
+    public partial class index : Form
+    {
+        public index()
+        {
+            InitializeComponent();
+        }
+    }
+}
