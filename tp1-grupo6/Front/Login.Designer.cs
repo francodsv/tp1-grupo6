b3cff3a0609fae83d6555d4aa30938e0bab473b8
Franco de San Vicente <francodv31@hotmail.com>
2022-05-12 17:39:04 -0300

    cambios

--- tp1-grupo6/Front/Login.Designer.cs
+++ tp1-grupo6/Front/Login.Designer.cs
@@ -57,6 +57,7 @@ namespace tp1_grupo6
             this.panel1.Controls.Add(this.label1);
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Registrar
             // 
